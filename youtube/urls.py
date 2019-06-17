b5353b05814e728685f21788169c7b29169ad3df
Nicholas Yin (cheyin) <cheyin@cisco.com>
2019-06-17 12:07:00 +0800

    uwsgi and nginx configuration file

--- youtube/urls.py
+++ youtube/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from videos import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('myadmin/', include('myadmin.urls')),
+    path('admin/', include('myadmin.urls')),
     path('users/', include('users.urls')),
     path('videos/', include('videos.urls')),
     path('', views.IndexView.as_view(), name='home'),  # 默认首页
