3f430e7373ab2e5e49d49f404291f349f3dc6094
Nicholas Yin (cheyin) <cheyin@cisco.com>
2019-06-19 14:46:29 +0800

    comment tip

--- videos/forms.py
+++ videos/forms.py
@@ -4,8 +4,8 @@ from comments.models import Comment
 
 
 class CommentForm(forms.ModelForm):
-    content = forms.CharField(error_messages={'required': '不能为空', },
-                              widget=forms.Textarea(attrs={'placeholder': '请输入评论内容'}))
+    content = forms.CharField(error_messages={'required': '评论不能为空', },
+                              widget=forms.Textarea(attrs={'placeholder': '请输入评论内容（200个字以内）'}))
 
     class Meta:
         model = Comment
